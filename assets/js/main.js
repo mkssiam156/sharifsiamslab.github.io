ab6777185263499416c7f850cd17ab75540c9e75
mkssiam156 <37828596+mkssiam156@users.noreply.github.com>
2018-03-27 14:45:58 +0600

    website for sharif siam's lab

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function($){
+    "use-strict"
+
+    $(document).ready(function(){
+         $(".galary-section").lightGallery(); 
+        $('#mainmenu').slicknav();
+        
+        
+        
+        
+    });
+ 
+})(jQuery)
+
